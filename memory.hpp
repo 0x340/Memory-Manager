6dc0650c173cf1cbfcb19b59b40b385bbbb2e8d4
0x340 <166537992+0x340@users.noreply.github.com>
2026-03-06 13:17:08 +0100

    Update memory.hpp

--- memory.hpp
+++ memory.hpp
@@ -4,47 +4,58 @@
 #include <Psapi.h>
 #include <string>
 #include <cstdint>
+#include <array>
+#include <memory>
+#include <format>
 
-using nt_read_fn  = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, ULONG, PULONG);
-using nt_write_fn = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, ULONG, PULONG);
+using nt_read_fn  = NTSTATUS( WINAPI* )( HANDLE, PVOID, PVOID, ULONG, PULONG );
+using nt_write_fn = NTSTATUS( WINAPI* )( HANDLE, PVOID, PVOID, ULONG, PULONG );
 
-nt_read_fn  build_read_syscall();
-nt_write_fn build_write_syscall();
-
-uint64_t find_process_id(std::string_view process_name);
-
-class memory_manager
-{
-public:
-    HANDLE proc_handle = nullptr;
-
-    memory_manager();
-    ~memory_manager();
-
-    bool open(std::string_view process_name);
-    void close();
-
-    uint64_t get_module_base(std::string_view module_name) const;
-
-    template<typename T>
-    T read(uintptr_t address) const
+namespace mm
     {
-        T val{};
-        m_read(proc_handle, reinterpret_cast<PVOID>(address), &val, sizeof(T), nullptr);
-        return val;
+    namespace internal
+        {
+        nt_read_fn  build_read_syscall( );
+        nt_write_fn build_write_syscall( );
+        std::uintptr_t find_process_id( std::string_view process_name );
+
+        class memory_manager
+            {
+            public:
+                HANDLE proc_handle = nullptr;
+
+                memory_manager( );
+                ~memory_manager( );
+
+                //helper fn
+                HANDLE          get_handle( );
+                bool            open( std::string_view process_name );
+                void            close( );
+                std::uintptr_t  get_module_base( std::string_view module_name ) const;
+                std::string     read_string( std::uintptr_t address ) const;
+                void            write_string( std::uintptr_t address, std::string_view value ) const;
+
+                template<typename T>
+                T read( std::uintptr_t address ) const
+                    {
+                    T val {};
+                    m_read( proc_handle, reinterpret_cast<PVOID>( address ), &val, sizeof( T ), nullptr );
+                    return val;
+                    }
+
+                template<typename T>
+                void write( std::uintptr_t address, T value ) const
+                    {
+                    m_write( proc_handle, reinterpret_cast<PVOID>( address ), &value, sizeof( T ), nullptr );
+                    }
+
+            private:
+                nt_read_fn  m_read  = nullptr;
+                nt_write_fn m_write = nullptr;
+
+                std::string read_raw_string( std::uintptr_t address ) const;
+            };
+        }
+
+    extern std::shared_ptr<internal::memory_manager> g_mm;
     }
-
-    template<typename T>
-    void write(uintptr_t address, T value) const
-    {
-        m_write(proc_handle, reinterpret_cast<PVOID>(address), &value, sizeof(T), nullptr);
-    }
-
-    std::string read_string(uintptr_t address) const;
-
-private:
-    nt_read_fn  m_read  = nullptr;
-    nt_write_fn m_write = nullptr;
-
-    std::string read_raw_string(uintptr_t address) const;
-};
