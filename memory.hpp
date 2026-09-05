a62ad2967996d71ce1aae20c4f2450c9e3e97977
lukiex <166537992+0x340@users.noreply.github.com>
2026-09-05 14:44:03 +0200

    Refactor memory access functions and license header

--- memory.hpp
+++ memory.hpp
@@ -1,35 +1,25 @@
 /*
-
-	MIT License
-
-	Copyright (c) 2026 0x340
-
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
-
-
+    MIT License
+    Copyright (c) 2026 0x340
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 
-
-
 #pragma once
-
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <Psapi.h>
@@ -39,58 +29,42 @@
 #include <memory>
 #include <format>
 
-using nt_read_fn  = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, ULONG, PULONG);
-using nt_write_fn = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, ULONG, PULONG);
+
+using nt_read_fn  = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, SIZE_T, PSIZE_T);
+using nt_write_fn = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, SIZE_T, PSIZE_T);
 
 namespace mm
 {
-    namespace internal
-    {
-        nt_read_fn build_read_syscall();
-        nt_write_fn build_write_syscall();
-        //
-        std::uintptr_t find_process_id(std::string_view process_name);
-        //
-        class memory_manager
-        {
-            public:
-                HANDLE proc_handle = nullptr;
-                //
-                memory_manager();
-                ~memory_manager();
 
-                // helper fn
-                HANDLE get_handle();
-                bool open(std::string_view process_name);
-                void close();
-                //
-                std::uintptr_t get_module_base(std::string_view module_name) const;
-                std::string read_string(std::uintptr_t address) const;
-                void write_string(std::uintptr_t address, std::string_view value) const;
-                //
-                template<typename T>
-                //
-                T read(std::uintptr_t address) const
-                {
-                    T val {};
-                    m_read(proc_handle, reinterpret_cast<PVOID>(address), &val, sizeof(T), nullptr);
-                    //
-                    return val;
-                }
-                //
-                template<typename T>
-                void write(std::uintptr_t address, T value) const
-                {
-                    m_write(proc_handle, reinterpret_cast<PVOID>(address), &value, sizeof(T), nullptr);
-                }
-                //
-            private:
-                nt_read_fn  m_read  = nullptr;
-                nt_write_fn m_write = nullptr;
-                //
-                std::string read_raw_string(std::uintptr_t address) const;
-            };
-        }
+    bool open_process(std::string_view process_name);
+    void close_process();
+    HANDLE get_process_handle(); // can be nullptr
+
+    std::uintptr_t get_module_base(std::string_view module_name);
+
+    std::string read_string(std::uintptr_t address);
+    void write_string(std::uintptr_t address, std::string_view value);
+
+    nt_read_fn  get_read_syscall();
+    nt_write_fn get_write_syscall();
+
+    template<typename T>
+
+    T read(std::uintptr_t address)
+    {
+        T val{};
+        nt_read_fn read_fn = get_read_syscall();
+        read_fn(get_process_handle(), reinterpret_cast<PVOID>(address), &val, sizeof(T), nullptr);
         //
-    extern std::shared_ptr<internal::memory_manager> g_mm;
-}
+        return {val};
+    }
+
+    template<typename T>
+
+    void write(std::uintptr_t address, T value)
+    {
+        nt_write_fn write_fn = get_write_syscall();
+        write_fn(get_process_handle(), reinterpret_cast<PVOID>(address), &value, sizeof(T), nullptr);
+    }
+
+} // mm
