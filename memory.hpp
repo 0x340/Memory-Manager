4fdc07ef8884a5cab127c2245da0ac007aa4ac0e
lukiex <166537992+0x340@users.noreply.github.com>
2026-09-06 14:25:14 +0200

    Update memory.hpp

--- memory.hpp
+++ memory.hpp
@@ -1,21 +1,21 @@
 /*
-    MIT License
-    Copyright (c) 2026 0x340
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+*    MIT License
+*    Copyright (c) 2026 0x340
+*    Permission is hereby granted, free of charge, to any person obtaining a copy
+*    of this software and associated documentation files (the "Software"), to deal
+*    in the Software without restriction, including without limitation the rights
+*    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*    copies of the Software, and to permit persons to whom the Software is
+*    furnished to do so, subject to the following conditions:
+*    The above copyright notice and this permission notice shall be included in all
+*    copies or substantial portions of the Software.
+*    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*    SOFTWARE.
 */
 
 #pragma once
@@ -30,7 +30,7 @@
 #include <format>
 #include <cstdint>
 
-using nt_read_fn  = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, SIZE_T, PSIZE_T);
+using nt_read_fn = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, SIZE_T, PSIZE_T);
 using nt_write_fn = NTSTATUS(WINAPI*)(HANDLE, PVOID, PVOID, SIZE_T, PSIZE_T);
 
 namespace memory
@@ -53,12 +53,20 @@ namespace memory
     nt_write_fn get_write_syscall();
 
     template<typename T>
+    bool read(std::uintptr_t address, T& out)
+    {
+        nt_read_fn read_fn = get_read_syscall();
+        NTSTATUS status = read_fn(get_process_handle(), reinterpret_cast<PVOID>(address), &out, sizeof(T), nullptr);
+        //
+        return {NT_SUCCESS(status)};
+    }
+
+    template<typename T>
     T read(std::uintptr_t address)
     {
         T val{};
-        nt_read_fn read_fn = get_read_syscall();
-        read_fn(get_process_handle(), reinterpret_cast<PVOID>(address), &val, sizeof(T), nullptr);
-
+        read(address, val);
+        //
         return {val};
     }
 
